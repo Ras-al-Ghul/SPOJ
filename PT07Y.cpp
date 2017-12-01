6bcefd9dbf123e6fa61e71b6c94183ec17e01d58
Ras-al-Ghul <dipankar1995@gmail.com>
2017-12-01 20:09:31 +0530

    Some more notes

--- PT07Y.cpp
+++ PT07Y.cpp
@@ -23,7 +23,7 @@
 // When you get to processing 2, 2, parent is 1, so don't add 1.
 // SAME THING COULD'VE been done here instead of while loop above
 // If you encounted an element which is already in the VISITED list,
-// it will necessarily mean that it is NOT A GRAPH.
+// it will necessarily mean that it IS A GRAPH.
 
 #include <iostream>
 #include <stack>
@@ -88,33 +88,51 @@ int main(){
 	// cout<<endl;
 	stack<pair<NODE*, int> > dfsstack; int value;
 	cur = new NODE;
+	// start from node with label 1
 	cur->val = 1; cur->next = NULL;
+	// first is value, second is parent
+	// push first node on to stack
 	dfsstack.push(pair<NODE*, int>(cur, -1));
 	while(!dfsstack.empty()){
+		// get value from stack top element
 		value = (dfsstack.top()).first->val;
+		// get parent value from stack top element
 		parent = (dfsstack.top()).second;
+		// point cur to relevant pointer - starts with 1
 		cur = arr[value];
+		// pop element from stack top
 		dfsstack.pop();
+		// if visited
 		if(cur->val==0){
+			// if it's parent doesn't equal itself, then it means that it is a cycle
+			// means that you're visiting the parent from a node other than the child
 			if(value != parent){
+				// it is a graph
 				flag = true;
 				break;
 			}
 			continue;
 		}
+		// to say that all nodes have been visited
 		count += 1;
+		// mark as visited
 		cur->val = 0;
 		// cout<<value<<" "<<parent<<endl;
 		cur=cur->next;
+		// traverse to the depth of the tree all the while adding to stack
 		while(cur != NULL){
+			// if 'cur' is parent of 'value - from above'
 			if(cur->val == parent)
+				// mark that parent's parent as itself
 				dfsstack.push(pair<NODE*, int>(cur, parent));
 			else
+				// this means that 'value' node is parent of cur
+				// i.e. 'value' node has added it to the stack
 				dfsstack.push(pair<NODE*, int>(cur, value));
 			cur = cur->next;
 		}
 	}
-
+	// standard edge case check - 'if connected' and 'no cycles'
 	if(count == nodes && !flag)
 		cout<<"YES"<<endl;
 	else
